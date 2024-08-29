977506120f96d8026c2cc3542d47095efb84ac25
Gonzalo Ariel Spesot <gonzalospesot7@gmail.com>
2024-08-29 16:54:01 -0300

    quick

--- algoritmos.py
+++ algoritmos.py
@@ -56,5 +56,46 @@ def bubbleSort(arr):
 
 #bubbleSort(arr)
 
+#QuickSort ( ordenamiento rapido )
 
+def quicksort(arr, start = 0, end = len(arr) - 1 ):
+    # Imprimimos la lista obtenida al principio (Desordenada)
+    print("El vector a ordenar con quick es:", arr)
+    
+    def quick(vectorquick, start = 0, end = len(arr) - 1):
+        if start >= end:
+            return
 
+        def particion(vectorquick, start = 0, end = len(vectorquick) - 1):
+            pivot = vectorquick[start]
+            menor = start + 1
+            mayor = end
+
+            while True:
+                # Si el valor actual es mayor que el pivot
+                # está en el lugar correcto (lado derecho del pivot) y podemos 
+                # movernos hacia la izquierda, al siguiente elemento.
+                # También debemos asegurarnos de no haber superado el puntero bajo, ya que indica 
+                # que ya hemos movido todos los elementos a su lado correcto del pivot
+                while menor <= mayor and vectorquick[mayor] >= pivot:
+                    mayor = mayor - 1
+                # Proceso opuesto al anterior            
+                while menor <= mayor and vectorquick[menor] <= pivot:
+                    menor = menor + 1
+                # Encontramos un valor sea mayor o menor y que este fuera del arreglo
+                # ó menor es más grande que mayor, en cuyo caso salimos del ciclo
+                if menor <= mayor:
+                    vectorquick[menor], vectorquick[mayor] = vectorquick[mayor], vectorquick[menor]
+                    # Continua el bucle
+                else:
+                    # Salimos del bucle
+                    break
+            vectorquick[start], vectorquick[mayor] = vectorquick[mayor], vectorquick[start]
+            return mayor
+        p = particion(vectorquick, start, end)
+        quick(vectorquick, start, p-1)
+        quick(vectorquick, p+1, end)
+    quick(arr)
+    print("El vector ordenado con quick es:", arr)
+
+quicksort(arr)
